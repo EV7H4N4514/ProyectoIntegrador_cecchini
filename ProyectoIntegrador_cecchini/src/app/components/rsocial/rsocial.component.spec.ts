87bd140979b490439f44d7b5ccf91c2534967aef
EV7H4N4514 <euge.cecchini@gmail.com>
2022-05-13 14:03:31 -0300

    Frontend

---
+++ ProyectoIntegrador_cecchini/src/app/components/rsocial/rsocial.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { RsocialComponent } from './rsocial.component';
+
+describe('RsocialComponent', () => {
+  let component: RsocialComponent;
+  let fixture: ComponentFixture<RsocialComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ RsocialComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(RsocialComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
